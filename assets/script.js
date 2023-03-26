7afe9425a813e4e636bc7b6d7b961d216b04ab32
Raymond <raymondjohnson@pursuit.org>
2023-03-26 16:22:43 -0400

    Not yet getting the elements created in the DOM.

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,61 @@
 // alert('javaScript connected!!')
 
-const form  = document.querySelector("form")
-const BASE_URL = "https://wttr.in/New+York?format=j1"
+//const form  = document.querySelector("form")
+const button = document.querySelector('#weather-button');
 
-form.addEventListener("submit", (event)=>{
+
+button.addEventListener("click", (event)=> {
   event.preventDefault();
+  let city = document.querySelector("#location").value
 
-  fetch(BASE_URL)
+  fetch(`https://wttr.in/${city}?format=j1`)
   .then((response) => response.json())
   .then((json) => {
-    json.results.forEach((result) =>{
-        displayTrivia(result)
-        showDifficulty(result)
+     displayWeather(json,city)
       })
-    })
-    .catch(showError(error));
-
-    let triviaArticles = document.querySelectorAll('article.card')
-    showCorrectAnswers(triviaArticles)
+    .catch(showError(Error));
 })
+
+const displayWeather = (weatherData,city) => {
+  console.log(weatherData)
+  // location.innerText = "";
+  const cityResults = document.querySelector('#city-results')
+  const cityName = document.createElement('h3')
+  const weatherResultList = document.createElement('ul')
+  
+  cityName.innerText = city;
+  cityResults.append(cityName)
+ 
+  const areaName = document.createElement('li')
+  areaText = weatherData.nearest_area[0].areaName[0].value;
+  areaName.innerHTML = `<strong>Region:</strong> ${areaText}`;
+  weatherResultList.append(areaName)
+  
+  const regionName = document.createElement('li')
+  regionText = weatherData.nearest_area[0].region[0].value;
+  regionName.innerHTML = `<strong>Region:</strong> ${regionText}`;
+  weatherResultList.append(regionName)
+
+  const country = document.createElement('li')
+  let countryText = weatherData.nearest_area.country[0].value;
+  country.innerHTML =  `<strong> Country:</strong> ${countryText}`
+  weatherResultList.append(country)
+  
+  const currentTemp = document.createElement('li')
+  let feelsLike = weatherData.current_condition[0].feelsLikeF.value;
+  currentTemp.innerHTML = `<strong> Currently:</strong> Feels Like ${feelsLike}°F` 
+  weatherResultList.append(currentTemp)
+  cityResults.append(weatherResultList)
+  // window.montreal = weatherData //<-- delete later
+  //let areaName = weatherData.nearest_area[0].area_name[0].value
+  //let regionName = json.region[0].region[0].value
+  //let country = json.region[0].country[0].value
+  //let temperature = json.current_condition[0].feelsLikeF[0].value
+  //For FEELS LIKEJohn... [copy and paste degree symbol]
+}
+
+const showError = (error) =>{
+
+}
 
 //https://wttr.in/New+York?format=j1
