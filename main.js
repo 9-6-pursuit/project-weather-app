726bef535434e485c292f85152dbe3b261e1318a
salvagebar <salvagebar@gmail.com>
2023-03-26 17:56:34 -0400

    Populating first item of test city

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
-let receivedPromise = fetch(`https://wttr.in/Montreal?format=j1`);
-const weatherBox = document.getElementById("weather-display");
+// let receivedPromise = fetch(`https://wttr.in/Montreal?format=j1`);
+let searchText = document.getElementById("location").value; // what the user inputs
+const weatherDisplayList = document.getElementById("weather-display-list");
+const weatherHeadline = document.getElementById("weather-location-headline");
+const searchForm = document.querySelector("form");
 
-receivedPromise
-  .then((response) => {
-    return response.json();
-  })
-  .then((json) => {
-    fillWeatherBox(json);
-  });
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-const fillWeatherBox = (json) => {
-  console.table(json);
+  let searchText = document.getElementById("location").value; // what the user inputs
+  document.getElementById("location").value = "";
+  let receivedPromise = `https://wttr.in/` + searchText + `?format=j1`;
+
+  fetch(receivedPromise)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+      fillWeatherDisplay(json);
+    });
+});
+
+// receivedPromise
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((json) => {
+//     fillWeatherDisplay(json);
+//   });
+
+const fillWeatherDisplay = (json) => {
+  const newListItem = document.createElement("li");
+  newListItem.textContent = `Feels Like: ${json.current_condition[0].FeelsLikeF}° F`;
+  weatherDisplayList.append(newListItem);
+  weatherHeadline.textContent = `${searchText}`;
+  console.log(json);
 };
+
+const getWeatherLocation = (location) => {};
