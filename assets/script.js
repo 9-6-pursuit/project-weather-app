5d3a7f3f99a8d77f53d1f6626d8028786076f17a
Raymond <raymondjohnson@pursuit.org>
2023-03-26 11:26:51 -0400

    2nd Commit--HTML basic setup complete

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,23 @@
 // alert('javaScript connected!!')
+
+const form  = document.querySelector("form")
+const BASE_URL = "https://wttr.in/New+York?format=j1"
+
+form.addEventListener("submit", (event)=>{
+  event.preventDefault();
+
+  fetch(BASE_URL)
+  .then((response) => response.json())
+  .then((json) => {
+    json.results.forEach((result) =>{
+        displayTrivia(result)
+        showDifficulty(result)
+      })
+    })
+    .catch(showError(error));
+
+    let triviaArticles = document.querySelectorAll('article.card')
+    showCorrectAnswers(triviaArticles)
+})
+
+//https://wttr.in/New+York?format=j1
