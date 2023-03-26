795b3b9631f4e7e9a3b8493c7774541629396fef
salvagebar <salvagebar@gmail.com>
2023-03-26 19:22:55 -0400

    Previous Search history list populating

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // let receivedPromise = fetch(`https://wttr.in/Montreal?format=j1`);
-let searchText = document.getElementById("location").value; // what the user inputs
+// let searchText = document.getElementById("location").value; // what the user inputs
 const weatherDisplayList = document.getElementById("weather-display-list");
+const weatherHistoryList = document.getElementById("weather-history-list");
 const weatherHeadline = document.getElementById("weather-location-headline");
 const searchForm = document.querySelector("form");
 
@@ -9,15 +10,15 @@ searchForm.addEventListener("submit", (event) => {
 
   let searchText = document.getElementById("location").value; // what the user inputs
   document.getElementById("location").value = "";
-  let receivedPromise = `https://wttr.in/` + searchText + `?format=j1`;
+  let baseURL = `https://wttr.in/` + searchText + `?format=j1`;
 
-  fetch(receivedPromise)
+  fetch(baseURL)
     .then((response) => {
       return response.json();
     })
     .then((json) => {
-      console.log(json);
-      fillWeatherDisplay(json);
+      fillWeatherDisplay(json, searchText);
+      fillHistoryDisplay(searchText);
     });
 });
 
@@ -29,12 +30,19 @@ searchForm.addEventListener("submit", (event) => {
 //     fillWeatherDisplay(json);
 //   });
 
-const fillWeatherDisplay = (json) => {
+const fillWeatherDisplay = (json, searchText) => {
+  weatherDisplayList.innerText = "";
   const newListItem = document.createElement("li");
   newListItem.textContent = `Feels Like: ${json.current_condition[0].FeelsLikeF}° F`;
   weatherDisplayList.append(newListItem);
-  weatherHeadline.textContent = `${searchText}`;
+  weatherHeadline.innerHTML = `${searchText}`;
   console.log(json);
 };
 
-const getWeatherLocation = (location) => {};
+const fillHistoryDisplay = (searchText) => {
+  const newListItem = document.createElement("li");
+  newListItem.textContent = `${searchText}`;
+  weatherHistoryList.append(newListItem);
+};
+
+// const getWeatherLocation = (location) => {};
