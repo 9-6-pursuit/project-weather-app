32a954d0a6389bf14166997d0dc75b937701d6e1
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-03-27 18:15:49 -0400

    Bottom section Added

--- main.js
+++ main.js
@@ -1,42 +1,83 @@
-const history = [];
 const searchButton = document.querySelector("button");
+let input = document.getElementById("city-input");
+let clickCount = 0;
 
 searchButton.addEventListener("click", (event) => {
     event.preventDefault();
-    let cityInput = document.getElementById("city-input");
-    history.unshift(cityInput.value);
-    let cityName = cityInput.value.replaceAll(" ", "+");
-    cityInput.value = "";
-
-    fetch(`https://wttr.in/${cityName}?format=j1`)
-    .then(response => response.json())
-    .then(json => {
-        showWeather(json);
-    })
+    if (input.value) {
+        let cityName = input.value.replaceAll(" ", "+");
+    
+        fetch(`https://wttr.in/${cityName}?format=j1`)
+        .then(response => response.json())
+        .then(json => showWeather(json))
+        .catch(showError);
+    }
 })
 
-function showWeather(weather) {
+function showWeather(json) {
     let weatherBox = document.querySelector(".weather-box");
-    weatherBox.innerHTML = "";
+    // weatherBox.innerHTML = "";
+    if (!clickCount) {
+        document.getElementById("empty").remove();
+        document.querySelector(".nolist").remove();
+        clickCount ++;
+    }
+    else {
+        let boxItems = document.querySelectorAll(".weather-box-item");
+        boxItems.forEach(item => item.remove());
+    }
 
     let header = document.createElement("h2");
-    header.textContent = history[0];
+    header.className = "weather-box-item";
+    header.textContent = input.value;
+    input.value = "";
     
     let area = document.createElement("li");
     area.className = "weather-box-item";
-    area.innerHTML = `<strong>Area:</strong> ${weather.nearest_area[0].areaName[0].value}`;
+    area.innerHTML = `<strong>Area:</strong> ${json.nearest_area[0].areaName[0].value}`;
 
     let region = document.createElement("li");
     region.className = "weather-box-item";
-    region.innerHTML = `<strong>Region:</strong> ${weather.nearest_area[0].region[0].value}`;
+    region.innerHTML = `<strong>Region:</strong> ${json.nearest_area[0].region[0].value}`;
 
     let country = document.createElement("li");
     country.className = "weather-box-item";
-    country.innerHTML = `<strong>Country:</strong> ${weather.nearest_area[0].country[0].value}`;
+    country.innerHTML = `<strong>Country:</strong> ${json.nearest_area[0].country[0].value}`;
 
     let currently = document.createElement("li");
     currently.className = "weather-box-item";
-    currently.innerHTML = `<strong>Currently:</strong> Feels like ${weather.current_condition[0].FeelsLikeF} °F`;
+    currently.innerHTML = `<strong>Currently:</strong> Feels like ${json.current_condition[0].FeelsLikeF}°F`;
 
-    weatherBox.append(header, area, region, country, currently);
+    weatherBox.prepend(header, area, region, country, currently);
+    let history = `${json.nearest_area[0].areaName[0].value} (${json.current_condition[0].FeelsLikeF}°F)`;
+
+    let searchList = document.querySelector(".search-list");
+    let listItem = document.createElement("li");
+    listItem.className = "sublist";
+    listItem.textContent = history;
+    searchList.append(listItem);
+
+    let days = ["Today", "Tomorrow", "Day After Tomorrow"];
+    for (let i = 0; i < 3; i++) {
+        let theDay = document.querySelector(`.day${i}`);
+        theDay.innerHTML = "";
+        let header = document.createElement("h3");
+        header.className = "bottom-text";
+        header.innerText = days[i];
+        let avgTemp = document.createElement("li");
+        avgTemp.className = "bottom-text";
+        avgTemp.innerHTML = `<strong>Avarage Temperature:</strong> ${json.weather[i].avgtempF}°F`;
+        let maxTemp = document.createElement("li");
+        maxTemp.className = "bottom-text";
+        maxTemp.innerHTML = `<strong>Max Temperature:</strong> ${json.weather[i].maxtempF}°F`;
+        let minTemp = document.createElement("li");
+        minTemp.className = "bottom-text";
+        minTemp.innerHTML = `<strong>Min Temperature:</strong> ${json.weather[i].mintempF}°F`;
+        theDay.append(header, avgTemp, maxTemp, minTemp);
+    }
+    document.querySelector(".bottom").style.border = "2px solid teal";
+}
+
+function showError(error) {
+    console.log(error);
 }
