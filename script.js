44a91d4045d8874bcb225f26bc988fd08d73dbf1
Phillip Andrew Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-03-27 17:28:55 -0400

    check script please John

--- script.js
+++ script.js
@@ -6,23 +6,28 @@ let BASE_URL = "https://wttr.in/" + nameofCity + "?format=j1";
 // https://wttr.in/brooklyn?format=j1
 // https://wttr.in/Montreal?format=j1
 function currentWeather(search) {
-
-    fetch(BASE_URL + search)
-        .then((response) => response.json())
-        .then(displayWeather)
-        .catch();
+    fetch("https://wttr.in/" + search + "?format=j1")
+    .then(response => { return response.json()})
+    .then(json => { displayWeather(json) })
+    .catch();
+    // fetch(BASE_URL + search)
+    //     .then((response) => response.json())
+    //     .then(displayWeather)
+    //     .catch();
 }
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    currentWeather(event.form)
-    document.getElementById("search").value = ""
+    currentWeather(document.getElementById("search").value = "")
+    // currentWeather(event.form)
+    // document.getElementById("search").value = ""
 });
 
 function displayWeather(result) {
     const section = document.querySelector("main.placeholder")
-    const h2 = document.createElement("h2")
-     h2.textContent = `${result.nearest_area.areaName}`
-     article.append(h2)
+    const article = document.createElement("article")
+    const h3 = document.createElement("h3")
+     h3.textContent = `${result.nearest_area.areaName}`
+     article.append(h3)
      section.append(article)
 }
