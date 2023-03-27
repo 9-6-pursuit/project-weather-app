150afcf2d3425ec7d9a9211feb5900682c373f75
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-03-26 21:41:29 -0400

    I directly added elements in the html folder, to make it easier to stylize the 3 days forecast

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ const weatherBox = document.getElementById("weather-box")
 const cityNameInput = document.getElementById("city-name-input")
 const searchButton = document.getElementById("search-button")
 const mainBody = document.getElementById("daBody")
-const div1 = document.createElement("divOne")
-div1.className = "wats-in-the-box"
-mainBody.append(div1)
-const div2 = document.createElement("divTwo")
-div2.className = "wats-in-the-box"
-mainBody.append(div2)
-const div3 = document.createElement("divThree")
-div3.className = "wats-in-the-box"
-mainBody.append(div3)
+
+const todayBox = document.getElementById("todayBox")
+const tomorrowBox = document.getElementById("tomorrowBox")
+const afterTomorrowBox = document.getElementById("afterTomorrowBox")
+
+// const div1 = document.createElement("divOne")
+// div1.className = "wats-in-the-box"
+// mainBody.append(div1)
+
+// const div2 = document.createElement("divTwo")
+// div2.className = "wats-in-the-box"
+// mainBody.append(div2)
+
+// const div3 = document.createElement("divThree")
+// div3.className = "wats-in-the-box"
+// mainBody.append(div3)
 
 searchButton.addEventListener("click", event => {
     let cityName = cityNameInput.value
@@ -56,79 +63,85 @@ const fillWeatherBox = (json, cityName) => {
    temperature.innerHTML = `<strong>Currently</strong> Feels like ${temperatureValue}°F`
    weatherBox.append(temperature)   
 
-    let today = document.createElement("ul") //starts here
+
+
+
+
+
+
+    let today = document.createElement("h3") //Today starts here
     today.innerHTML = `<strong>Today</strong>`
-    div1.append(today)
+    todayBox.append(today)
 
    let todayAvrgTempValue = json.weather[0].avgtempF
-   let todayAvrgTemp = document.createElement("ul")
+   let todayAvrgTemp = document.createElement("li")
    todayAvrgTemp.className = "threeDays"
    todayAvrgTemp.innerHTML = `<strong>Average Temperature:</strong> ${todayAvrgTempValue}°F`
-   div1.append(todayAvrgTemp)
+   todayBox.append(todayAvrgTemp)
     console.log(todayAvrgTempValue)
 
    let todayMaxTempValue = json.weather[0].maxtempF
-   let todayMaxTemp = document.createElement("ul")
+   let todayMaxTemp = document.createElement("li")
    todayMaxTemp.className = "threeDays"
    todayMaxTemp.innerHTML = `<strong>Max Temperature:</strong> ${todayMaxTempValue}°F`
-   div1.append(todayMaxTemp)
+   todayBox.append(todayMaxTemp)
     console.log(todayMaxTempValue)
 
    let todayMinTempValue = json.weather[0].mintempF
-   let todayMinTemp = document.createElement("ul")
+   let todayMinTemp = document.createElement("li")
    todayMinTemp.className = "threeDays"
    todayMinTemp.innerHTML = `<strong>Min Temperature:</strong> ${todayMinTempValue}°F`
-   div1.append(todayMinTemp)
-    console.log(todayMinTempValue) //ends here
+   todayBox.append(todayMinTemp)
+    console.log(todayMinTempValue) //Today ends here
 
-    let tomorrow = document.createElement("ul")
+    let tomorrow = document.createElement("h3") //Tomorrow starts here
     tomorrow.innerHTML = `<strong>Tomorrow</strong>`
-    div2.append(tomorrow)
+    tomorrowBox.append(tomorrow)
 
    let tomorrowAvrgTempValue = json.weather[1].avgtempF
-   let tomorrowAvrgTemp = document.createElement("ul")
+   let tomorrowAvrgTemp = document.createElement("li")
    tomorrowAvrgTemp.className = "threeDays"
    tomorrowAvrgTemp.innerHTML = `<strong>Average Temperature:</strong> ${tomorrowAvrgTempValue}°F`
-   div2.append(tomorrowAvrgTemp)
+   tomorrowBox.append(tomorrowAvrgTemp)
     console.log(tomorrowAvrgTempValue)
 
    let tomorrowMaxTempValue = json.weather[1].maxtempF
-   let tomorrowMaxTemp = document.createElement("ul")
+   let tomorrowMaxTemp = document.createElement("li")
    tomorrowMaxTemp.className = "threeDays"
    tomorrowMaxTemp.innerHTML = `<strong>Max Temperature:</strong> ${tomorrowMaxTempValue}°F`
-   div2.append(tomorrowMaxTemp)
+   tomorrowBox.append(tomorrowMaxTemp)
     console.log(tomorrowMaxTempValue)
 
    let tomorrowMinTempValue = json.weather[1].mintempF
-   let tomorrowMinTemp = document.createElement("ul")
+   let tomorrowMinTemp = document.createElement("li")
    tomorrowMinTemp.className = "threeDays"
    tomorrowMinTemp.innerHTML = `<strong>Min Temperature:</strong> ${tomorrowMinTempValue}°F`
-   div2.append(tomorrowMinTemp)
-    console.log(tomorrowMinTempValue) //Ends here
+   tomorrowBox.append(tomorrowMinTemp)
+    console.log(tomorrowMinTempValue) //Tomorrow Ends here
 
-    let afterTomorrow = document.createElement("ul")
+    let afterTomorrow = document.createElement("h3") //Day After Tomorrow starts here
     afterTomorrow.innerHTML = `<strong>Day After Tomorrow</strong>`
-    div3.append(afterTomorrow)
+    afterTomorrowBox.append(afterTomorrow)
 
    let afterTomorrowAvrgTempValue = json.weather[2].avgtempF
-   let afterTomorrowAvrgTemp = document.createElement("ul")
+   let afterTomorrowAvrgTemp = document.createElement("li")
    afterTomorrowAvrgTemp.className = "threeDays"
    afterTomorrowAvrgTemp.innerHTML = `<strong>Average Temperature:</strong> ${afterTomorrowAvrgTempValue}°F`
-   div3.append(afterTomorrowAvrgTemp)
+   afterTomorrowBox.append(afterTomorrowAvrgTemp)
     console.log(afterTomorrowAvrgTempValue)
 
    let afterTomorrowMaxTempValue = json.weather[2].maxtempF
-   let afterTomorrowMaxTemp = document.createElement("ul")
+   let afterTomorrowMaxTemp = document.createElement("li")
    afterTomorrowMaxTemp.className = "threeDays"
    afterTomorrowMaxTemp.innerHTML = `<strong>Max Temperature:</strong> ${afterTomorrowMaxTempValue}°F`
-   div3.append(afterTomorrowMaxTemp)
+   afterTomorrowBox.append(afterTomorrowMaxTemp)
     console.log(afterTomorrowMaxTempValue)
 
    let afterTomorrowMinTempValue = json.weather[2].mintempF
-   let afterTomorrowMinTemp = document.createElement("ul")
+   let afterTomorrowMinTemp = document.createElement("li")
    afterTomorrowMinTemp.className = "threeDays"
    afterTomorrowMinTemp.innerHTML = `<strong>Min Temperature:</strong> ${afterTomorrowMinTempValue}°F`
-   div3.append(afterTomorrowMinTemp)
-    console.log(afterTomorrowMinTempValue)
+   afterTomorrowBox.append(afterTomorrowMinTemp)
+    console.log(afterTomorrowMinTempValue) //Day After Tomorrow ends here
 
 }
